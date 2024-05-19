5ee460149d700d8bc11296c0e3ebf728676d5e62
José del Rey <jreyortega@al.uloyola.es>
2024-05-19 18:00:32 +0200

    exercise 16 done

---
+++ src/examples/sources-collisions.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        name:'player',
+        type: 'texture',
+        path: '../assets/player/Prota_Frente.png'
+    },
+    {
+        name:'enemy',
+        type: 'texture',
+        path: '../assets/player/enemy.png'
+    },
+    {
+        name:'background',
+        type: 'texture',
+        path: '../assets/background/dungeon.jpg'
+    }
+]    
