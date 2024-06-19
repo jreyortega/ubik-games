3dfe353e2f15c70c82d49b9a79b93911cdef3dc9
José del Rey <jreyortega@al.uloyola.es>
2024-06-19 17:59:31 +0200

    dsaj

--- src/procedural_generator/main.js
+++ src/procedural_generator/main.js
@@ -4,9 +4,10 @@ import sources from './sources-dungeon.js';
 import * as CANNON from 'cannon-es';
 import * as THREE from 'three';
 import { inicializar_mapa } from './inicializacion_mapa.js';
-import Player from './Classes/Player.js'
-import Enemy from './Classes/Enemy.js'
-import Portal from './Classes/Portal.js'
+import Player from './Classes/Player.js';
+import Enemy from './Classes/Enemy.js';
+import Portal from './Classes/Portal.js';
+
 const ubik = new Ubik({ cameraType: 'orthographic' });
 
 // // Gravity
@@ -19,52 +20,45 @@ ubik.scene.add(ambientLight);
 // Add orbit controls
 ubik.camera.addOrbitControls();
 
+// Generate dungeon
 const tileSize = 2;
-const dungeonGenerator = new CorridorFirstDungeonGenerator({ x: 0, y:0 }, 100, 3, true);
+const dungeonGenerator = new CorridorFirstDungeonGenerator({ x: 0, y: 0 }, 100, 3, true);
 const dungeon = dungeonGenerator.runProceduralGeneration();
-
 
 //#######INICIALIZACION DE OBJETOS
 //-------Inicializacion del objeto character-----------
-const tam= new CANNON.Vec3(tileSize/2,tileSize/2,tileSize/2)
+const tam = new CANNON.Vec3(tileSize / 2, tileSize / 2, tileSize / 2);
 const character = ubik.createObject();
-character.position.set(0,0,0);
+character.position.set(0, 0, 0);
 ubik.addComponent(
     character,
     'rigidbody',
     ubik.physics.createBody({
         mass: 1,
         shape: new CANNON.Box(tam)
-}))
+    })
+);
 
 //--------Inicializción del objeto portal------------
-const portal= ubik.createObject();
+const portal = ubik.createObject();
 
 //--------Inicialización de los objetos enemigos---------
 const enemies = [];
 
 //--------Inicialización del objeto llave--------
-const key=ubik.createObject()
+const key = ubik.createObject();
 
-inicializar_mapa(dungeon,tileSize,ubik,sources,character,enemies,portal,key,THREE,CANNON);
+inicializar_mapa(dungeon, tileSize, ubik, sources, character, enemies, portal, key, THREE, CANNON);
 
-console.log("character-------------",portal)
-console.log("character-------------",portal)
-console.log("character-------------",portal)
-console.log("character-------------",portal)
-console.log("character-------------",portal)
-console.log("character-------------",portal)
-console.log("character-------------",portal)
+console.log("character-------------", portal);
 
-const player = new Player(0, 0,character,ubik);
-const Portal_1= new Portal(portal,ubik)
-
+const player = new Player(0, 0, character, ubik);
+const Portal_1 = new Portal(portal, ubik);
 
 ubik.update = (dt) => {
-   ubik.physics.update(dt, ubik.objects); // Actualizar física
-   player.update(dt)
-   Portal_1.update(dt)
-}
+    ubik.physics.update(dt, ubik.objects); // Actualizar física
+    player.update(dt);
+    Portal_1.update(dt);
+};
 
 ubik.start();
-
